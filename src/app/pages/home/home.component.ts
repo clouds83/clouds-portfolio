f5b2d45b657b3613124c7a035a007969099b1d25
Claudio Godoy <clouds83@gmail.com>
2023-01-18 16:27:34 -0300

    update

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  verticalViewPortHeight!: String;
+  slides!: Object[];
 
+  constructor() {}
+
+  ngOnInit(): void {
+    this.slides = [
+      {
+        title: 'My Balance',
+        text: 'Web application for financial control created using the Angular framework.',
+        background: 'mb-bg',
+        hostedUrl: 'https://my-balance-clouds83.vercel.app/reports',
+        githubUrl: 'https://github.com/clouds83/my-balance2',
+        imgUrl: 'assets/img/my-balance-home_552.png',
+      },
+      {
+        title: 'CLIPZ',
+        text: 'Short video platform for the gaming niche.',
+        background: 'blue',
+        hostedUrl: 'https://game-clips-clouds83.vercel.app/',
+        githubUrl: 'https://github.com/clouds83/game-clips',
+        imgUrl: 'assets/img/clipz-home_552.png',
+      },
+      {
+        title: 'MovieDB',
+        text: 'Website that lists movies using a public API',
+        background: 'red',
+        hostedUrl: 'https://movie-db-clouds83.vercel.app/',
+        githubUrl: 'https://github.com/clouds83/movie-db',
+        imgUrl: 'assets/img/moviedb-home_552.png',
+      },
+    ];
+  }
 }
